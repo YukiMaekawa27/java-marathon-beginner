003946018faad0d5da15123d88efa692f91f6ea1
yuki.maekawa <yuki.maekawa@rakus.co.jp>
2019-05-27 16:49:49 +0900

    UserDaoファイルを作成しました

---
+++ src/main/java/com/example/repository/UserDao.java
@@ -0,0 +1,33 @@
+package com.example.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.User;
+
+@Repository
+public class UserDao {
+	private final static RowMapper<User> USER_ROW_MAPPER = (rs, i) -> {
+		User user = new User();
+		user.setId(rs.getInt("id"));
+		user.setName(rs.getString("name"));
+		user.setAge(rs.getInt("age"));
+		user.setAddress(rs.getString("address"));
+		return user;
+	};
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	public User load(Integer id) {
+		String sql = "select * from info where id=:id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		User user = template.queryForObject(sql, param, USER_ROW_MAPPER);
+		return user;
+	}
+
+}
