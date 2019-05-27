9abb4985e04db8f9accbab0906cc3afb5e186488
yuki.maekawa <yuki.maekawa@rakus.co.jp>
2019-05-27 14:19:54 +0900

    1問目を解き終わりました

---
+++ src/main/java/com/example/controller/NameReceiveController.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/name")
+public class NameReceiveController {
+
+	@RequestMapping("")
+	public String index() {
+		return "inputname";
+	}
+
+	@RequestMapping("/output")
+	public String output(String name, Model model) {
+		model.addAttribute("name", name);
+
+		return "output";
+	}
+
+}
